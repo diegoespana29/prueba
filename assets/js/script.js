071b01a8a99c294e326e2f9c28fb8c4625e2add6
Diego España <diego.espa.lacroix@hotmail.com>
2021-11-27 11:49:36 -0300

    v3

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var Nombre="Diego España";
+var curso="Desarrollo Web";
+
+var NotaHtml1=4;
+var NotaHtml2=5;
+var NotaHtml3=4;
+var NotaCss1=3;
+var NotaCss2=4;
+var NotaCss3=5;
+var NotaJavascript1=2;
+var NotaJavascript2=6;
+var NotaJavascript3;
+var NotaFinalHTML;
+var NotaFinalCss;
+var NotaFinalJavascript;
+
+function CalcularNotaHTML()
+{
+  NotaFinalHTML=NotaHtml1+NotaHtml2+NotaHtml3;
+  NotaFinalHTML=NotaFinalHTML/3;
+alert(NotaFinalHTML);
+
+}
+function CalcularNotaHTML()
+{
+  NotaFinalCss=NotaCss1+NotaCss2+NotaCss3;
+  NotaFinalCss=NotaFinalCss/3;
+alert(NotaFinalCss);
+
+}
